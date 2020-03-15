fca5885d504049b6afa43bce2af53fd30fccd7cf
Crew Spence <csspence123@gmail.com>
2020-03-15 09:18:41 -0500

    Wrote working solution for moving_zeros_to_the_end.

--- moving_zeros_to_the_end.js
+++ moving_zeros_to_the_end.js
@@ -4,6 +4,21 @@ Write an algorithm that takes an array and moves all of the zeros to the end, pr
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 */
 
-var moveZeros = function (arr) {
-  // TODO: Program me
+const moveZeros = (arr) => {
+  const outputArr = [];
+  const zerosArr = [];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] !== 0) {
+      outputArr.push(arr[i]);
+    } else {
+      zerosArr.push(arr[i]);
+    }
+  }
+  if(outputArr.length > 0) {
+    for(let j = 0; j < zerosArr.length; j++) {
+      outputArr.push(zerosArr[j]);
+    }
+  }
+
+  return outputArr;
 }
